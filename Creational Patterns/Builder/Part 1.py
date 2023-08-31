919ebc2347a8df705c0d2bfae63eff5aed460da1
Ramsudharsan Manoharan <ramsudharsan.manoharan@optym.com>
2023-08-31 16:56:27 +0530

    Buider Pattern

---
+++ Creational Patterns/Builder/Part 1.py
@@ -0,0 +1,34 @@
+class HtmlElement:
+    indent_size = 2
+
+    def __init__(self, name="", text="") -> None:
+        self.name = name
+        self.text = text
+        self.elements = []
+
+
+class HtmlBuilder:
+    def __init__(self, root_name):
+        self.root_name = root_name
+        self.__root = HtmlElement(root_name)
+
+    def add_child(self, child_name, child_text):
+        self.__root.elements.append(HtmlElement(child_name, child_text))
+
+    def add_child_fluent(self, child_name, child_text):
+        self.__root.elements.append(HtmlElement(child_name, child_text))
+        return self
+
+    def __str__(self) -> str:
+        return str(self.__root)
+    
+    @staticmethod
+    def create(root_name):
+        return HtmlBuilder(root_name)
+    
+
+builder = HtmlBuilder.create('ul')
+builder.add_child_fluent('li', 'hello')
+builder.add_child_fluent('li', 'world')
+print("Ordinary builder:")
+print(builder)
