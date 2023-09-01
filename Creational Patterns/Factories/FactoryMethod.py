39e578ddd00c761685eecd53703a402f659be59b
Ramsudharsan Manoharan <ramsudharsan.manoharan@optym.com>
2023-09-01 16:56:14 +0530

    Factory Method

---
+++ Creational Patterns/Factories/FactoryMethod.py
@@ -0,0 +1,23 @@
+from math import cos, sin
+
+
+class Point:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+    @staticmethod  # factory method
+    def new_cartesian_point(x, y):
+        return Point(x, y)
+
+    @staticmethod
+    def new_polar_point(rho, theta):
+        return Point(rho * cos(theta), rho + sin(theta))
+
+
+if __name__ == "__main__":
+    p = Point(1, 2)
+    p2 = Point.new_cartesian_point(2, 3)
+    p3 = Point.new_polar_point(30, 45)
+
+    print(p3.x)
