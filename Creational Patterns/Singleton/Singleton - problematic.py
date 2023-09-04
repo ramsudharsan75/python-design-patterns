13679d00084a87be8a28769f2d1ee043628d488c
Ramsudharsan Manoharan <ramsudharsan.manoharan@optym.com>
2023-09-04 22:40:33 +0530

    Singleton

---
+++ Creational Patterns/Singleton/Singleton - problematic.py
@@ -0,0 +1,18 @@
+class Database:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Database, cls).__new__(cls, *args, **kwargs)
+
+        return cls._instance
+    
+    def __init__(self) -> None:
+        print("Loading a database from file")
+
+
+if __name__ == "__main__":
+    d1 = Database()
+    d2 = Database()
+
+    print(d1 == d2)
